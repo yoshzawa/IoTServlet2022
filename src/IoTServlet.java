36da21813c3cf009e29385243dd07b921b6d6c79
yoshzawa <yoshzawa@gmail.com>
2022-07-05 23:07:59 +0900

    first commit

---
+++ src/IoTServlet.java
@@ -0,0 +1,48 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IoTServlet
+ */
+@WebServlet("/iot")
+public class IoTServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Connected.");
+		Map<String, String[]> params = request.getParameterMap();
+		for(String key : params.keySet()) {
+			System.out.println("key["+key+"]");
+			for(String value : params.get(key)) {
+				System.out.println("value["+key+"]");
+			}
+		}
+		Enumeration<String> headerName = request.getHeaderNames();
+		for(String key : Collections.list(headerName)) {
+			System.out.println("header["+key+"]");
+			String value = request.getHeader(key);
+			System.out.println("value["+value+"]");
+		}
+		String content = request.getReader().readLine();
+		System.out.println(content);
+		
+	}
+
+}
---
+++ src/IoTServlet2.java
@@ -0,0 +1,69 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.*;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class IoTServlet
+ */
+@WebServlet("/iot2")
+public class IoTServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		ServletContext application = this.getServletContext();
+		 ArrayList<String> list = (ArrayList<String>)application.getAttribute("list");
+		 if(list==null) {
+			 return;
+		 }
+
+		 for(String s :list) {
+			out.println("<H3>"+s+"</H3>");
+		 }
+		 
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String host = request.getHeader("host");
+		System.out.println("host["+host+"]");
+
+		String user = request.getHeader("user");
+		System.out.println("user["+user+"]");
+
+		String content = request.getReader().readLine();
+		System.out.println(content);
+		
+		
+		ServletContext application = this.getServletContext();
+
+		 ArrayList<String> list = (ArrayList<String>)application.getAttribute("list");
+		 if(list == null) {
+			 list = new ArrayList<String>();
+		 }
+		 list.add("host["+host+"] user["+user+"] content=["+content+"]");
+		 application.setAttribute("list", list);
+		
+		
+		
+	}
+
+}
