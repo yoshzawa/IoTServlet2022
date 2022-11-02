89106f9335a31f45369c88e16b5fc5866f4186aa
yoshzawa <yoshzawa@gmail.com>
2022-11-02 15:39:19 +0900

    課題２用のセットを作成

--- src/IoTServlet2.java
+++ src/IoTServlet2.java
@@ -1,22 +1,15 @@
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Reader;
-import java.util.*;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Map;
+import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class IoTServlet
